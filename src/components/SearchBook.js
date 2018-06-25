f3790cb5978418345d8a5a09e14f9bb4ec61c69a
richard <richlip@googlemail.com>
2018-06-25 09:22:38 +0200

    beautify code

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,45 +1,53 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import React, {
+    Component
+} from 'react'
+import {
+    Link
+} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
-
 class SearchBook extends Component {
-  
-state = {searchedBooks: []}
-
-  search = (event) => {
-    if (event.target.value.length < 1) {
-      return true;
+    state={
+        searchedBooks: []
     }
-    
-    BooksAPI.search(event.target.value, 25).then((books) => {
-      if (books.error === undefined) {
-        this.setState({searchedBooks: books});
-      } else {
-        this.setState({searchedBooks: []});
-      }
-    });
-  }
-
-  render () {
-    const { searchedBooks } = this.state;
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to="/">Close</Link>
-          <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" onChange={this.search}/>
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            {searchedBooks.map((book) => (
-              <Book key={book.id} book={book} onChangeShelf={this.props.onShelfSelect} />
-            ))}
-          </ol>
-        </div>
-      </div>
-    )
-  }
+    search=(event) => {
+        if (event.target.value.length < 1) {
+            return true;
+        }
+        BooksAPI.search(event.target.value, 25).then((books) => {
+            if (books.error === undefined) {
+                this.setState({
+                    searchedBooks: books
+                });
+            } else {
+                this.setState({
+                    searchedBooks: []
+                });
+            }
+        });
+    }
+    render() {
+        const {
+            searchedBooks
+        }=this.state;
+        return ( < div className="search-books" > < div className="search-books-bar" > < Link className="close-search"
+            to="/" > Close < /Link> < div className="search-books-input-wrapper" > < input type="text"
+            placeholder="Search by title or author"
+            onChange={
+                this.search
+            }
+            /> < /div> < /div> < div className="search-books-results" > < ol className="books-grid" > {
+                searchedBooks.map((book) => ( < Book key={
+                        book.id
+                    }
+                    book={
+                        book
+                    }
+                    onChangeShelf={
+                        this.props.onShelfSelect
+                    }
+                    />))
+            } < /ol> < /div> < /div>)
+    }
 }
 export default SearchBook
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+import {
+    BrowserRouter
+} from 'react-router-dom'
+ReactDOM.render( < BrowserRouter > < App / > < /BrowserRouter>, document.getElementById('root'))
